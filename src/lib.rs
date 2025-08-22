9d0ef0f066058fc0357765f27c2f850681809f75
Mihir Mahesh <emailmihirmahesh@gmail.com>
2025-08-22 18:40:56 -0500

    Created initial Chip8 struct

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,17 @@
+struct Chip8 {
+    memory: [u8; 1000],
+    stack: [u16; 16], // Stored as 16 16 bit values
+    
+    // Registers the program can directly access and modify (Except for F)
+    gen_purpose_registers: [u8; 16],
+    register_i: u16, // Special 16 bit register
 
+    delay_timer: u8,
+    sound_timer: u8,
+
+    
+    program_counter: u16,
+    stack_pointer: u8, 
+
+    time: u8 // Will increase by 1 at a rate of 60 hz
+}
